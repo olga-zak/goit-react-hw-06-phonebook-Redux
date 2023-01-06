d890adbdb0c36cb1e0fdb5b46004d9db4b017fa1
olga-zak <olgaliam@gmail.com>
2023-01-06 16:53:01 +0200

    add Redux Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore, combineReducers } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
 import { devToolsEnhancer } from '@redux-devtools/extension';
 // import { rootReducer } from './reducer';
@@ -9,6 +11,15 @@ const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // Создаем расширение стора чтобы добавить инструменты разработчика
 const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+//export const store = createStore(rootReducer, enhancer);
+export const store = createStore(persistedReducer, enhancer);
+export const persistor = persistStore(store);
